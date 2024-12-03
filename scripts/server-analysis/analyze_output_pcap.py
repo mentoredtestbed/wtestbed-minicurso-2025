843ea5315c90762ad7de0a20e923c0c7b1d3b687
Khalil Santana <khalil@ksantana.net>
2024-12-03 18:04:12 -0300

    Add server analysis (WIP)

---
+++ scripts/server-analysis/analyze_output_pcap.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import argparse
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+
+
+
+def read_data(file_path):
+    data = pd.read_csv(file_path)
+    data['Timestamp'] = pd.to_datetime(data['Timestamp'], format='%H:%M:%S.%f')
+    return data
+
+
+
+def compute_metrics(data, freq='S'):
+    data.set_index('Timestamp', inplace=True)
+
+    # Start counting the timestamp from 0
+    data.index = data.index - data.index[0]
+    throughput = data['Packet Size'].resample(freq).sum()  # Sum of packet sizes
+    packet_counts = data['Packet Size'].resample(freq).count()  # Count of packets
+    return throughput, packet_counts
+
+
+
+def plot_metrics(throughput, packet_counts, freq, fontsize, expected_time):
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+    # convert expected_time to Timedelta
+    expected_time = pd.Timedelta(seconds=expected_time)
+    color = 'tab:red'
+    ax1.set_xlabel('Tempo (segundos)', fontsize=fontsize, fontweight='bold')
+    ax1.set_ylabel('Vazão (MB)', color=color, fontsize=fontsize, fontweight='bold')
+
+    # Plot x axis as time
+    # "Recover" the initial part not captured in the pcap file
+    # data_x = throughput.index/1000000000
+    data_x = throughput.index
+    print(max(throughput.index))
+    print(expected_time)
+    if max(data_x) < expected_time:
+        data_x = data_x + (expected_time - max(data_x))
+   
+    ax1.plot(data_x/1000000000, throughput.values/(2**20), color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:blue'
+    ax2.set_ylabel('Número de Pacotes', color=color, fontsize=fontsize, fontweight='bold')
+
+    data_x = packet_counts.index
+    if max(data_x) < expected_time:
+        data_x = data_x + (expected_time - max(data_x))
+
+    ax2.plot(data_x/1000000000, packet_counts.values, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    # Avoid using scientific notation
+    ax1.get_xaxis().get_major_formatter().set_scientific(False)
+    ax1.get_yaxis().get_major_formatter().set_scientific(False)
+    ax2.get_yaxis().get_major_formatter().set_scientific(False)
+
+    # Set font size
+    ax1.tick_params(axis='both', which='major', labelsize=fontsize)
+    ax2.tick_params(axis='both', which='major', labelsize=fontsize)
+
+    plt.savefig(f'../output_{freq}.png', dpi=300)
+
+
+def filter_to_experiment_duration(data, timestamp_start, duration):
+    # Convert unix timestamp to hours, like: 19:33:32.399970
+    timestamp_start = pd.Timestamp(timestamp_start, unit='s')
+    data = data[data.index >= timestamp_start]
+    data = data[data.index <= timestamp_start + pd.Timedelta(seconds=duration)]
+    return data
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--input_file', dest='input_file', required=True)
+    parser.add_argument('-s', '--fontsize', dest='fontsize', type=int, default=12, help='Font size for the plot')
+    parser.add_argument('-t', '--expected_time', dest='time', type=int, default=60, help='Experiment expected time in seconds')
+    parser.add_argument('-u', '--unix_timestamp', dest='unix_timestamp', type=int, help='Unix timestamp of experiment start')
+
+    args = parser.parse_args()
+    file_path = args.input_file
+    expected_time = args.time
+    # freq_options = {'S': 'Second', 'T': 'Minute', 'H': 'Hour', 'D': 'Day', 'M': 'Month', 'Y': 'Year'}
+
+    freq_options = {'S': 'Second'}
+
+    data = read_data(file_path)
+    # data = filter_to_experiment_duration(data, args.unix_timestamp, args.time)
+
+    for freq, label in tqdm(freq_options.items(), desc="Computing and plotting metrics"):
+        throughput, packet_counts = compute_metrics(data, freq=freq)
+        plot_metrics(throughput, packet_counts, label, args.fontsize, expected_time)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/server-analysis/experiment_analyzer.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+pushd /app/scripts/server-analysis
+mkdir .tmp_exp_analyzer
+
+# if not two args
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <experiment_file> <experiment_duration>"
+    exit 1
+fi
+
+# If file not exist
+if [ ! -f pcap_to_csv ]; then
+    
+    echo "Compiling pcap_to_csv..."
+    make
+fi
+
+EXPFILE=$1
+EXPERIMENT_DURATION=$2
+echo "Extracting $EXPFILE..."
+tar -zxf $EXPFILE -C .tmp_exp_analyzer --wildcards '*server*' # TODO: UNCOMMENT THIS
+
+tar -zxf $EXPFILE -C .tmp_exp_analyzer --wildcards '*MENTORED_READY.txt'
+
+START_TIMESTAMP=$(cat .tmp_exp_analyzer/*MENTORED_READY.txt)
+rm .tmp_exp_analyzer/*MENTORED_READY.txt
+
+echo "Extracted $EXPFILE"
+
+echo "Running experiment analyzer..."
+cd .tmp_exp_analyzer
+for f in */*.tar; do mkdir ${f}.files; tar -xf "$f" -C ${f}.files; done
+
+# Find recursevely all .pcap files and iterate over them
+for f in $(find . -name "*.pcapng"); do
+    echo "Converting $f to csv..."
+    # tshark -r $f -T fields -e frame.time_epoch -e frame.len -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.flags -e tcp.window_size -e tcp.analysis.ack_rtt -e tcp.analysis.ack_rtt -e tcp.analysis.bytes_in_flight
+    ../pcap_to_csv $f packets.csv # TODO UNCOMMENT THIS
+    set -x
+    python3 ../analyze_output_pcap.py -s 16 -f packets.csv -t $EXPERIMENT_DURATION
+done
+
+cd ..
+echo "Experiment analyzer finished"
+rm -rf .tmp_exp_analyzer
+popd
